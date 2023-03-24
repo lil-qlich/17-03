9f474f2c0241fdcac7d4278ecac0c2d8da1a5cd5
lil-qlich <127847622+lil-qlich@users.noreply.github.com>
2023-03-24 08:48:20 +0300

    ыфвфыв

---
+++ target/debug/deps/new_store-ce4e5e703a8e8391.d
@@ -0,0 +1,10 @@
+c:\Users\sajan\OneDrive\Рабочий стол\Колледж\АР\17-03\target\debug\deps\new_store-ce4e5e703a8e8391.rmeta: src\main.rs src\sclad.rs src\buy_products.rs src\akaunt.rs src\order.rs src\psih.rs
+
+c:\Users\sajan\OneDrive\Рабочий стол\Колледж\АР\17-03\target\debug\deps\new_store-ce4e5e703a8e8391.d: src\main.rs src\sclad.rs src\buy_products.rs src\akaunt.rs src\order.rs src\psih.rs
+
+src\main.rs:
+src\sclad.rs:
+src\buy_products.rs:
+src\akaunt.rs:
+src\order.rs:
+src\psih.rs:
